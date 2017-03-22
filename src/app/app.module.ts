1f1e73b87b9555b253e8327a8c1aeff2f53dc3fa
MargoT <mtereshc@gmail.com>
2017-03-22 14:28:03 -0400

    Add components and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotesComponent } from './components/notes/notes.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NoteComponent } from './components/note/note.component';
+import { AddNoteComponent } from './components/add-note/add-note.component';
+import { EditNoteComponent } from './components/edit-note/edit-note.component';
+
+
+const appRoutes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'notes', component:NotesComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotesComponent,
+    NavbarComponent,
+    NoteComponent,
+    AddNoteComponent,
+    EditNoteComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
